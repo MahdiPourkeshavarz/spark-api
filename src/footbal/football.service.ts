73a90ba97db5f5ed95cb9c73d3717863a837100e
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-09-29 11:49:23 +0330

    feat(football): cache system implemented

--- src/footbal/football.service.ts
+++ src/footbal/football.service.ts
@@ -8,6 +8,7 @@ import {
   HttpStatus,
   Logger,
   NotFoundException,
+  Inject,
 } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
@@ -19,6 +20,8 @@ import { MatchResultDto } from './dto/match-result.dto';
 import { allEuropeanLeaguesData, teamData } from './constants/teams';
 import { ConfigService } from '@nestjs/config';
 import Fuse from 'fuse.js';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class FootballService {
@@ -29,6 +32,7 @@ export class FootballService {
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {
     axiosRetry(this.httpService.axiosRef, {
       retries: 3,
@@ -46,9 +50,35 @@ export class FootballService {
     return results;
   }
 
+  async searchTeam(name: string) {
+    const fuse = new Fuse(allEuropeanLeaguesData, {
+      keys: ['name', 'shortName'],
+      threshold: 0.7,
+    });
+
+    const result = fuse.search(name);
+
+    const teamId = result && result[0].item.id;
+
+    if (teamId) {
+      const match = await this.fetchLastMatch(result[0].item);
+      return match;
+    } else {
+      throw NotFoundException;
+    }
+  }
+
   private async fetchLastMatch(team: TeamDto): Promise<MatchResultDto> {
     const dateFrom = format(subMonths(new Date(), 3), 'yyyy-MM-dd');
     const dateTo = format(new Date(), 'yyyy-MM-dd');
+    const cacheKey = `last-match-${team.id}`;
+
+    const cached = (await this.cacheManager.get(cacheKey)) as MatchResultDto;
+
+    if (cached) {
+      return cached;
+    }
+
     try {
       const response = await firstValueFrom(
         this.httpService
@@ -88,7 +118,7 @@ export class FootballService {
       );
       const lastMatch = matches[0];
 
-      return {
+      const res = {
         home: lastMatch.homeTeam.name,
         away: lastMatch.awayTeam.name,
         score: `${lastMatch.score.fullTime.home} - ${lastMatch.score.fullTime.away}`,
@@ -96,26 +126,12 @@ export class FootballService {
         date: lastMatch.utcDate,
         competition: lastMatch.competition.name,
       };
+
+      await this.cacheManager.set(cacheKey, res, 240 * 1000);
+
+      return res;
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-
-  async searchTeam(name: string) {
-    const fuse = new Fuse(allEuropeanLeaguesData, {
-      keys: ['name', 'shortName'],
-      threshold: 0.7,
-    });
-
-    const result = fuse.search(name);
-
-    const teamId = result && result[0].item.id;
-
-    if (teamId) {
-      const match = await this.fetchLastMatch(result[0].item);
-      return match;
-    } else {
-      throw NotFoundException;
     }
   }
 }
